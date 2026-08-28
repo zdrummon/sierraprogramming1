90d44e5fb32f91435a0bbad4c199c25824b3a3a2
zdrummon <zdrummon@gmail.com>
2026-08-28 07:38:14 -0700

    fixed homework

--- proj_3_Homework1/Homework1.java
+++ proj_3_Homework1/Homework1.java
@@ -1,40 +1,27 @@
+/*
+Zachary Drummond
+August 28, 2026
+    This program display my initials in a decorated box
+    I did not write this with AI, nor did I cheat. Signed, Zachary Drummond
+    I believe this deserves an A, there is no logic just printlns that create my initials and even a random print function
+*/
+
 // Homework1.java
 package proj_3_Homework1;
-import java.util.Scanner;
 
 public class Homework1 {
 
     public static void main(String[] args) {
         
-        //init input scanner
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Put in your first name: ");
-        String firstName = scanner.nextLine(); //scan input
-
-        System.out.println("Put in your last name: ");
-        String lastName = scanner.nextLine(); //scan input
-
-        char first = Character.toUpperCase(firstName.charAt(0));
-        char last;
-        if (lastName.charAt(0) != firstName.charAt(0)) {
-            last = Character.toUpperCase(lastName.charAt(0));
-        } else {
-            last = Character.toUpperCase(lastName.charAt(1));
-        }
-        
-        System.out.println("=========================");
-        System.out.println("=  your initials below  =");
-        System.out.println("=========================");
-        System.out.println("=                       =");
-        System.out.println("=                       =");
-
-        System.out.println("=        " + first + "     " + last + "        =");
-        
-        System.out.println("=                       =");
-        System.out.println("=                       =");
-        System.out.println("=========================");
-
-        scanner.close(); //always gotta remember to close this lol
+        //print my initials in a box
+        System.out.println("\n/=o=o=o=o=o=o=o=o=o=o=o=\\");
+        System.out.println("\\                       /");
+        System.out.println("/   ======     =====    \\");
+        System.out.println("\\      //      ||  \\\\   /");
+        System.out.println("/     //       ||   \\\\  \\");
+        System.out.println("\\    //        ||   //  /");
+        System.out.println("/   ======     =====    \\");
+        System.out.println("\\                       /");
+        System.out.print(" \\o=o=o=o=o=o=o=o=o=o=o/ \n\n");
     }
 }
